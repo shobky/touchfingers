81263b50618a6549cb8d5ea1eacea1c471f4f590
shobky <shobkyy@gmail.com>
2023-06-04 07:09:28 +0300

    '[temp]reset-btn-not-working'

--- components/restart/index.tsx
+++ components/restart/index.tsx
@@ -7,17 +7,17 @@ import ts from "typescript";
 
 export default function Restart() {
   const { state, dispatch } = useTypingContext();
-  const input = document.getElementById("input");
+  // const input = document.getElementById("input");
   const router = useRouter();
 
   const resetGame = () => {
     state.words.sort(() => Math.random() - 0.5);
     dispatch({ type: "RESET_GAME" });
 
-    if (input) {
-      // @ts-ignore
-      input.value = "";
-    }
+    // if (input) {
+    //   // @ts-ignore
+    //   input.value = "";
+    // }
   };
 
   const refreshForNow = () => {
