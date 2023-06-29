19719914524fa656310aaeee57481fa5dcf81803
shobky <shobkyy@gmail.com>
2023-06-30 02:58:15 +0300

    'build-err'

--- components/restart/index.tsx
+++ components/restart/index.tsx
@@ -5,13 +5,15 @@ import React from "react";
 
 export default function Restart() {
   const { state, dispatch } = useTypingContext();
-  const input = document.getElementById("input")  as HTMLInputElement
+  const input = document
+    ? (document.getElementById("input") as HTMLInputElement)
+    : "";
 
   const resetGame = () => {
     dispatch({ type: "RESET_GAME" });
     state.words.sort(() => Math.random() - 0.5);
     if (input) {
-      input.value = ""
+      input.value = "";
     }
   };
 
